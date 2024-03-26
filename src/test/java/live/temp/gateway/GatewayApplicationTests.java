55f42e90f6528fb7561fb020cd1d1681f5e16c96
nakta-y <nakta.y.biz@gmail.com>
2024-03-26 16:18:35 +0900

    [Chore] : First Project Settings

---
+++ src/test/java/live/temp/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package live.temp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
