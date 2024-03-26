2bf392276ef373bf9dba9ba1bb629dd30dd154b8
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-03-26 21:16:06 +0900

    Add: AuthorizationNotFoundException.java 추가

---
+++ src/main/java/live/smoothing/gateway/exception/AuthorizationNotFoundException.java
@@ -0,0 +1,8 @@
+package live.smoothing.gateway.exception;
+
+public class AuthorizationNotFoundException extends RuntimeException{
+    public AuthorizationNotFoundException(String message) {
+        super(message +": Authorization not found in header");
+    }
+}
+
