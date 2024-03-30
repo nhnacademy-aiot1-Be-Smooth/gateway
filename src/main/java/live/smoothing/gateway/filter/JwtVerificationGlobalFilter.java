2a506b37715f59d1a368064ab4c38b0e8494ff0a
nakta-y <nakta.y.biz@gmail.com>
2024-03-30 22:16:18 +0900

    [Feat] : Jwt 토큰 검증 예외 처리 구현

--- src/main/java/live/smoothing/gateway/filter/JwtVerificationGlobalFilter.java
+++ src/main/java/live/smoothing/gateway/filter/JwtVerificationGlobalFilter.java
@@ -1,7 +1,10 @@
 package live.smoothing.gateway.filter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.jsonwebtoken.MalformedJwtException;
 import live.smoothing.gateway.config.GlobalFilterProperties;
+import live.smoothing.gateway.exception.InvalidJsonFormatException;
+import live.smoothing.gateway.exception.JwtTokenInvalidException;
 import live.smoothing.gateway.jwt.util.JwtCode;
 import live.smoothing.gateway.jwt.util.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -30,28 +34,23 @@ public class JwtVerificationGlobalFilter implements GlobalFilter, Ordered {
             return chain.filter(exchange);
         }
 
-        String accessToken = String.valueOf(exchange.getAttributes().getOrDefault("accessToken", null));
-        String refreshToken = String.valueOf(exchange.getAttributes().getOrDefault("refreshToken", null));
+        String accessToken = String.valueOf(exchange.getAttributes().get("accessToken"));
 
-        // TODO: exception 처리
         try {
             JwtCode jwtCode = jwtTokenProvider.validateToken(accessToken);
 
             if (jwtCode == JwtCode.INVALID) {
-                throw new RuntimeException();
+                throw new JwtTokenInvalidException(HttpStatus.UNAUTHORIZED, "유효하지 않은 Jwt 토큰 입니다.");
             }
 
             String userId = jwtTokenProvider.getUserId(accessToken);
-
-            if (jwtCode == JwtCode.EXPIRED) {
-                throw new RuntimeException();
-            }
-
             exchange.getAttributes().put("userId", userId);
             return chain.filter(exchange);
 
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            throw new InvalidJsonFormatException(HttpStatus.BAD_REQUEST, "Jwt 토큰의 json 포맷이 잘못 되었습니다.");
+        } catch (MalformedJwtException e) {
+            throw new JwtTokenInvalidException(HttpStatus.UNAUTHORIZED, "유효하지 않은 Jwt 토큰 입니다.");
         }
     }
 
