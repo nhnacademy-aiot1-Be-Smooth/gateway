f3ecae8a20b937dec134337f2cad8d647421de76
Jiyoon <bystar1687@nate.com>
2024-04-19 17:44:15 +0900

    [Fix]: JwtParser 수정

--- src/main/java/live/smoothing/gateway/config/JwtConfig.java
+++ src/main/java/live/smoothing/gateway/config/JwtConfig.java
@@ -2,10 +2,14 @@ package live.smoothing.gateway.config;
 
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import live.smoothing.gateway.jwt.prop.JwtProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
 
 /**
  * Jwt Token 관련 설정 클래스
@@ -25,8 +29,9 @@ public class JwtConfig {
      */
     @Bean
     public JwtParser jwtParser() {
+        Key key = new SecretKeySpec(jwtProperties.getSecret().getBytes(), SignatureAlgorithm.HS256.getJcaName());
         return Jwts.parserBuilder()
-                .setSigningKey(jwtProperties.getSecret().getBytes())
+                .setSigningKey(key)
                 .build();
     }
 }
